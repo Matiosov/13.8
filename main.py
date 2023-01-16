35edc45b751bee0cd922d8b43768d31c4f5706f0
Matiosov <122042891+Matiosov@users.noreply.github.com>
2023-01-16 11:50:43 +0300

    ВНЕСЕНЫ ИЗМИНЕНИЯ В ФАЙЛ

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ for i in range(ticket):
     elif 18 <= age < 25:
         cost = cost + 990
     else: cost = cost + 1390
-    if ticket > 3:
-        cost = cost * 0.9
+if ticket > 3:
+    cost = cost * 0.9
 print('Итого'+ ' ' + str(round(cost)) + ' ' + 'рублей')
-
